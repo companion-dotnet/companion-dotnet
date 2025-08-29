08e340c7ff8da1bb71b9f728c4a014ad84b133fa
Stephan Bauer <stephan.bauer@arcanix.at>
2025-08-29 15:12:56 +0200

    Fix docs

--- src/Signaling/benchmark/Companion.Signaling.Benchmark/Program.cs
+++ src/Signaling/benchmark/Companion.Signaling.Benchmark/Program.cs
@@ -3,7 +3,7 @@ using Companion.Signaling.Benchmark;
 
 /* 
  * The benchmarks provided do not state anything about the performance of this library. Whether
- * it is a enhancement or not is fully dependent on the use case. When working with a lot of 
+ * it is an enhancement or not is fully dependent on the use case. When working with a lot of 
  * components (e.g. buttons, inputs, etc.) or very complex ones, the overhead of the signaling
  * system is far less than the performance gains from avoiding unnecessary renders.
  * 
